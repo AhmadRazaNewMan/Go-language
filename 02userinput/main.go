bc248999ab0be996615e306388e7b2c01d9d759f
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-05-16 13:40:24 +0500

    Adding UserInputs

---
+++ 02userinput/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	welcome := "Welcome to user input file"
+	fmt.Println(welcome)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter Your understanding in go langaugae:")
+
+	//Comma Ok or Comma Err syntacs in go language there is no try and catch the go treats the error also as an error
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for Rating", input)
+
+}
